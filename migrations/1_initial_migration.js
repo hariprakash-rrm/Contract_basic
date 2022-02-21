c2edf6a36c790db968efb0a3fb111575c62e7fcf
santhiya <santhiyaarthi456@gmail.com>
2022-02-21 11:02:39 +0530

    first commitsf

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -4,7 +4,7 @@ const Loopy = artifacts.require("Loopy");
 const tokenName = "Loopy";
 const symbol = "LP"; 
 const decimal= 18;  
-const totalSupply= "100000000000000000000000000"; 
+const totalSupply= "1000000000000000000000000"; 
 
 module.exports = function (deployer) {
   deployer.deploy(Loopy, tokenName, symbol, decimal, totalSupply);
